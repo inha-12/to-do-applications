0cb4e0c5d750318d3844a4ca095ed58d866b7e70
inha-12 <inharohilla@gmial.com>
2024-08-31 03:05:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDo from './components/ToDo'
+
+const App = () => {
+  return (
+    <div className='bg-purple-400 grid py-4 min-h-screen'>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
